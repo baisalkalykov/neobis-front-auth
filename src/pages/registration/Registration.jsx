c6f8ef625a13663c5a94248b2099bd2b7fdae28b
mrroot <baisalkalykov69@gmail.com>
2024-03-03 23:37:10 +0600

    formik

--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -1,47 +1,36 @@
 import React from 'react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import './Registration.scss'
-import {useForm} from 'react-hook-form'
 import boy from '../../assets-images/Photo background.png'
 import eye from '../../assets-images/eye.svg'
 import eyeoff from '../../assets-images/eye-close.svg'
 import arow from '../../assets-images/arow.svg'
+import './Registration.scss'
+import { Formik } from 'formik'
+import * as yup from 'yup' 
+
 
 
 function Registration() {
+
+  const validationsSchema= yup.object().shape({
+    email:yup.string().email('должно быть строкой').required('Обязательно'),
+    login:yup.string().typeError('должно быть строкой').required('Обязательно'),
+    password:yup.string().typeError('должно быть строкой').required('Обязательно'),
+    confirmPassword:yup.string().oneOf([yup.ref('password')],'Пароли не совпадают').required('Обязательно')
+  })
   const [password,setPassword]=useState('password')
   const[show,setShow]=useState(eyeoff)
-  const[inputValue,setInputValue]=useState('')
-
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-
-  const handleEyeClic=()=>{
-    if(password==='password'){
-      setShow(eye)
-      setPassword('text')
-    }
-    else{
-      setShow(eyeoff)
-      setPassword('password')
-    }
-  }
-
-  const{
-    register,
-    formState:{
-     errors
-    },
-    handleSubmit
-  }=useForm()
-
- const onSubmit=(data)=>{
-   alert(JSON.stringify(data))
- }
+    const handleEyeClic=()=>{
+     if(password==='password'){
+       setShow(eye)
+       setPassword('text')
+     }
+     else{
+       setShow(eyeoff)
+       setPassword('password')
+   }
+   }
   return (
     
     <div className='register'>
@@ -52,66 +41,100 @@ function Registration() {
       </div>
       </Link>
       <div className="register__container">
-      <div className="register__img">
-        <img src={boy} alt="boy" className='regisret__images' />
-      </div>
-      <form className='register__form' onSubmit={handleSubmit(onSubmit)}>
-        <p className='register__form-p'>Создать аккаунт <br /> Lorby</p>
-      <input 
-      type="text"
-      placeholder='Введи адрес почты'
-      {...register('gmail',{
-        required:true
-      })}
-      onChange={handleInputChange}
-      className='register__form-input'
-      style={{
-        color: inputValue.length < 8 ? 'red' : inputValue.length > 8 ? 'green' : 'black',
+        <div className="register__img">
+          <img src={boy} alt="" />
+        </div>
+      <Formik
+        initialValues={{
+        email: '',
+        login: '',
+        password: '',
+        confirmPassword: ''
       }}
-      />
-      <input 
-      type="text"
-      placeholder='Придумай логин'
-      {...register('Придумай логин')}
-      className='register__form-input'
-      />
-      <label className='register__form-password'>
-      <input 
-      type={password}
-      placeholder='Создай пароль'
-      {...register('Создай пароль')}
-      className='register__form-input'
-      />
-      <img src={show} 
-      alt="show" 
-      className='register__form-img' 
-      onClick={handleEyeClic} />
-      </label>
-      <div className="register__form-text">
-      {inputValue.length < 8 && (
-         <li style={{ color: inputValue.length < 8 ? 'red' : inputValue.length > 8 ? 'green' : 'black' }}  
-         className="register__form-li">От 8 до 15 символов</li>
-      )}
-       
-        <li className="register__form-li">Строчные и прописные буквы</li>
-        <li className="register__form-li">Минимум 1 цифра</li>
-        <li className="register__form-li">Минимум 1 спецсимвол (!, ", #, $...)</li>
-      </div>
-      <label className='register__form-password'>
-      <input 
-        type={password}
-        placeholder='Повтори пароль'
-       {...register('Повтори пароль')}
-       className='register__form-input'
-       
-      />
-       <img src={show} 
-      alt="show"
-       className='register__form-img'
-       onClick={handleEyeClic}  />
-      </label>
-      <button className='register__btn'>Далее</button>
-      </form>
+        validateOnBlur
+        onSubmit={(values) => {
+        console.log(values);
+        }}
+        validationSchema={validationsSchema}
+      >
+      {({ values, errors, touched, handleChange, handelBlur, isValid, handleSubmit, dirty }) => (
+         <div className="register__form">
+           <p className='register__form-p'>Создать аккаунт <br /> Lorby</p>
+           <label  htmlFor={`email`} className='register__form-lable'>
+           <input
+              type="email"
+              placeholder='Введи адрес почты'
+              name={`email`}
+              className='register__form-input'
+              onChange={handleChange}
+              onBlur={handelBlur}
+              value={values.email}
+            />
+            
+          {touched.email && errors.email && <p className='register__from-eror'>{errors.email}</p>}
+           </label>
+
+           <label htmlFor={`login`} className='register__form-lable'>
+           <input
+              type="text"
+              placeholder='Придумай логин'
+              name={`login`}
+              className='register__form-input'
+              onChange={handleChange}
+              onBlur={handelBlur}
+              value={values.login}
+            />
+            
+          {touched.login && errors.login && <p className='register__from-eror'>{errors.login}</p>}
+           </label>
+
+            <label htmlFor={`password`} className='register__form-pass-lable'>
+            <input
+              type="password"
+              placeholder='Создай пароль'
+              name={`password`}
+              className='register__form-input'
+              onChange={handleChange}
+              onBlur={handelBlur}
+              value={values.password}
+            />
+              <img src={show} 
+                alt="show" 
+                className='register__form-img' 
+                onClick={handleEyeClic} />
+          {touched.password && errors.password && <p className='register__from-eror'>{errors.password}</p>}
+
+            </label>
+
+            <label htmlFor={`confirmPassword`} className='register__form-pass-lable'>
+            <input
+              type="password"
+              placeholder='Повтори пароль'
+              name={`confirmPassword`}
+              className='register__form-input'
+              onChange={handleChange}
+              onBlur={handelBlur}
+              value={values.confirmPassword}
+            />
+             <img src={show} 
+                alt="show" 
+                className='register__form-img' 
+                onClick={handleEyeClic} />
+          {touched.confirmPassword && errors.confirmPassword && <p className='register__from-eror'>{errors.confirmPassword}</p>}
+
+            </label>
+
+            <button
+              disabled={!isValid && !dirty}
+              onClick={handleSubmit}
+              type={`submit`}
+              className='register__btn'
+            >
+            Далее
+            </button>
+          </div>
+        )}
+    </Formik>
       </div>
     </div>
   )
